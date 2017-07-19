c4461e1b92b41b692367dba11c8aa3b707e708a3
Victor Doss <victor.doss@gmail.com>
2017-07-19 14:32:08 -0700

    initial checkin

---
+++ spec/codesidestory-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodesidestoryAtomView from '../lib/codesidestory-atom-view';
+
+describe('CodesidestoryAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
